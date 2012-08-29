23bdf7784a77b0d5a2878c24c34ede08aa20cf5e
Timofey.Kirillov <timofey.kirillov@flant.ru>
2012-08-29 14:02:18 +0400

    The Big Reorganization.

--- test/sync_client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo python -i -c 'from test.sync_client import *' $@
---
+++ tests/config/console/gamma.py
@@ -0,0 +1,23 @@
+cfg = {
+    "server": {
+        "name": "gamma",
+        "interface": "127.0.0.1",
+        "port": 1102
+    },
+
+    "database": {
+        "dbenv_homedir": "./test/db/gamma",
+        "dbfile": "dlz.db"
+    },
+
+    "peers": {
+        "alpha": {
+            "host": "127.0.0.1",
+            "port": 1100
+        },
+        "beta": {
+            "host": "127.0.0.1",
+            "port": 1101
+        }
+    }
+}
